6e2b8dc222cff00d1376042c8a0b6259e8435263
maitatduy <maitatduy.work@gmail.com>
2026-04-10 18:02:07 +0700

    feat(backend): create Auth Controller (Register, Login, Logout) with JWT using cookie-parser and asyncHandler

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -10,8 +12,11 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // Routes
+app.use("/api/auth", authRoutes);
 app.get("/api/health", (req, res) => {
   res.status(200).json({ status: "ok", message: "Server is running!" });
 });
